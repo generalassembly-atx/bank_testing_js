6012ff425b3afb9732276659449275993dd5abe3
Zach Gaines <zrgaines@mtu.edu>
2016-06-16 21:06:49 -0500

    Finished bank test

--- spec/bank_spec.js
+++ spec/bank_spec.js
@@ -5,11 +5,13 @@ describe("Bank", function() {
   describe("#createAccount", function() {
 
     it("adds an accounts to the bank's 'accounts' object", function() {
-
+      var newAccount = Bank('Zach');
+      expect(newAccount).toBe(this.name)
     });
 
     it("sets the new account's balance to the amount specified", function() {
-
+      var initAmount = Bank(133700);
+      expect(initAmount).toBe(this.amount)
     });
 
   });
@@ -17,7 +19,8 @@ describe("Bank", function() {
   describe("#deposit", function() {
 
     it("adds the appropriate amount to the named account", function() {
-
+      var addAmount = Bank(500);
+      expect(addAmount).toBe(this.amount);
     });
 
   });
@@ -25,7 +28,8 @@ describe("Bank", function() {
   describe("#withdraw", function() {
 
     it("withdraws the appropriate amount from the named account", function() {
-
+      var withAmount = Bank(500);
+      expect(withAmount).toBe(this.amount);
     });
 
   });
@@ -33,7 +37,8 @@ describe("Bank", function() {
   describe("#balance", function() {
 
     it("returns the current balance of the named account", function() {
-
+      var currAmount = this.amount;
+      expect(currAmount).toBe(this.amount)
     });
 
   });
