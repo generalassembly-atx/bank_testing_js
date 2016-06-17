77180bcb15f949047a9605f470859c6cfb7515da
Chris Osborne <chris.a.osborne@gmail.com>
2016-06-17 08:56:00 -0500

    first commit

--- spec/bank_spec.js
+++ spec/bank_spec.js
@@ -5,11 +5,13 @@ describe("Bank", function() {
   describe("#createAccount", function() {
 
     it("adds an accounts to the bank's 'accounts' object", function() {
-
+        var newAccount = Bank('BoA');
+        expect(newAccount).toBe(this.accountName);
     });
 
     it("sets the new account's balance to the amount specified", function() {
-
+        var newAmount = Bank(200);
+        expect(newAmount).toBe(this.amount);
     });
 
   });
@@ -17,7 +19,8 @@ describe("Bank", function() {
   describe("#deposit", function() {
 
     it("adds the appropriate amount to the named account", function() {
-
+        var deposit = Bank('BoA', 200);
+        expect(deposit).toBe(this.amount);
     });
 
   });
@@ -25,7 +28,8 @@ describe("Bank", function() {
   describe("#withdraw", function() {
 
     it("withdraws the appropriate amount from the named account", function() {
-
+        var withdraw = Bank('BoA', 150);
+        expect(withdraw).toBe(this.amount);
     });
 
   });
