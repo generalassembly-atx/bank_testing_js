37e6cc87e052a76504e2ff61a6a6407d1618fe51
Alfredo Amengual <amengual.alfredo@gmail.com>
2016-06-17 08:55:52 -0500

    wasnt able to finish, got 1st part functioning

--- spec/bank_spec.js
+++ spec/bank_spec.js
@@ -5,11 +5,13 @@ describe("Bank", function() {
   describe("#createAccount", function() {
 
     it("adds an accounts to the bank's 'accounts' object", function() {
-
+      var addAccount = Bank("Bank of America");
+      expect(addAccount).toBe(this.name);
     });
 
     it("sets the new account's balance to the amount specified", function() {
-
+    var accountAmount = Bank(200);
+    expect(accountAmount).toBe(this.amount);
     });
 
   });
@@ -17,7 +19,9 @@ describe("Bank", function() {
   describe("#deposit", function() {
 
     it("adds the appropriate amount to the named account", function() {
-
+      var deposit = Bank("BOA", 200);
+      var currentAmount = this.accounts + 200;
+      expect(this.amount).toBe(200);
     });
 
   });
