bcb0956ba53feefa6ddd49ede46953b49b4ae455
Michael Malleske <mmalleske@gmail.com>
2016-06-17 09:22:13 -0500

    Michael Malleske HW d14

--- spec/bank_spec.js
+++ spec/bank_spec.js
@@ -5,11 +5,13 @@ describe("Bank", function() {
   describe("#createAccount", function() {
 
     it("adds an accounts to the bank's 'accounts' object", function() {
-
+      var newAccount = Bank.prototype.createAccount("checking", 300);
+      expect( newAccount ).toBe( "checking" );
     });
 
     it("sets the new account's balance to the amount specified", function() {
-
+      //var
+      expect( newAccount.amount ).toBe( 300 );
     });
 
   });
@@ -17,7 +19,8 @@ describe("Bank", function() {
   describe("#deposit", function() {
 
     it("adds the appropriate amount to the named account", function() {
-
+        var newDeposit = Bank.deposit("checking", 45);
+        console.log(newDeposit);
     });
 
   });
